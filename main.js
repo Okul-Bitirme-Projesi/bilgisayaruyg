6cb59b937fa6b448474367ef54c86933c6392d78
Murat Can <44754026+SlenderShout@users.noreply.github.com>
2022-03-08 19:49:20 +0300

    BilgisayarUygulaması

---
+++ main.js
@@ -0,0 +1,90 @@
+// Modules to control application life and create native browser window
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  Notification,
+  webContents,
+} = require("electron");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + "./kakashi.ico",
+    webPreferences: {
+      preload: `${__dirname}/renderer.js`,
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL("http://localhost:18420/index.aspx");
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+function createMainMenu() {
+  const template = [
+    /*{ //! Sayfada Arama sistemi Eklenecek.
+      role: "Arama",
+      accelerator: process.platform === "darwin" ? "Ctrl+F" : "Ctrl+F",
+      click: () => {
+        // console.log(webContents.getFocusedWebContents()); //? Düzeltilecek
+      },
+    },*/
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+const NOTIFICATION_TITLE = "Test Uygulaması";
+const NOTIFICATION_BODY = "Uygulama Açıldı";
+
+function showNotification() {
+  new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+  }).show();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", async () => {
+  await createWindow();
+  await createMainMenu();
+  await showNotification();
+});
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
